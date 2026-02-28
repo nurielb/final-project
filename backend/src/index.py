438be1f42506bb1c538ed5f073623534a3e63267
Nuriel Benjamin <nurielbenjamin@gmail.com>
2026-02-28 20:16:42 +0200

    added logger

--- backend/src/index.py
+++ backend/src/index.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 app = FastAPI()
-
+logger = logging.getLogger(__name__)
 
 @app.get("/")
 def read_root():
+    logger.info('Handled GET / request')
+    
     return {
         "Hello": "World",
         "Branch": "development",
@@ -13,4 +19,6 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
+    logger.info(f'Handled GET /items/{item_id} request')
+    
     return {"item_id": item_id, "q": q}
